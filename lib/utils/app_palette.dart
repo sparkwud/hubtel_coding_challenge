80f89983318698157c2748fa8198070ed1a0873d
Kelvin Agbenyo <kelvinagbenyo@gmail.com>
2024-09-22 15:59:12 +0000

    update app drawables

--- lib/utils/app_palette.dart
+++ lib/utils/app_palette.dart
@@ -5,14 +5,13 @@ class AppPalette {
   static const Color black = Colors.black;
   static const Color unselectedColor = Color(0xffCBCBCB);
   static const Color selectedColor = Colors.black;
-  static const textGray = Color.fromRGBO(156, 171, 184, 1);
-  static const navTextGray = Color.fromRGBO(203, 203, 203, 1);
-  static const lightGray = Color(0xFFEEEEF0);
-  static const borderGray = Color.fromRGBO(230, 234, 237, 1);
-  static const successGreenBg = Color.fromRGBO(219, 247, 224, 1);
-  static const textGreen = Color.fromRGBO(112, 224, 131, 1);
-  static const textRed = Color.fromRGBO(153, 35, 29, 1);
-  static const failedRedBg = Color.fromRGBO(253, 176, 172, 1);
-  static const teal = Color.fromRGBO(1, 199, 177, 1);
-  static const lightTeal = Color.fromRGBO(204, 243, 239, 1);
+  static const textGray = Color(0xFF9CABB8);
+  static const lightGray = Color(0xFFE6EAED);
+  static const borderGray = Color(0xFFE6EAED);
+  static const greenBackground = Color(0xFFDBF7E0);
+  static const greenText = Color(0xFF70E083);
+  static const redText = Color(0xFF99231D);
+  static const redBackground = Color(0xFFFDB0AC);
+  static const teal = Color(0xFF01C7B1);
+  static const lightTeal = Color(0xFFCCF3EF);
 }
